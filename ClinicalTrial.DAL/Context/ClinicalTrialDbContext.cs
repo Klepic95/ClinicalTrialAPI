71b93ffda003369159f9f209bbfe5383337fc3de
Klepic95 <klepicpredrag@gmail.com>
2024-12-11 16:12:41 +0100

    Updated connection with the database

--- ClinicalTrial.DAL/Context/ClinicalTrialDbContext.cs
+++ ClinicalTrial.DAL/Context/ClinicalTrialDbContext.cs
@@ -1,23 +1,48 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+﻿using ClinicalTrial.DAL.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClinicalTrial.DAL.Context;
 
 public class ClinicalTrialDbContext : DbContext
 {
-    public ClinicalTrialDbContext(DbContextOptions<ClinicalTrialDbContext> options) : base(options) { }
+    public DbSet<ClinicalTrialDTO> ClinicalTrials { get; set; }
 
-    public DbSet<Models.ClinicalTrialDTO> ClinicalTrials { get; set; }
+    public ClinicalTrialDbContext()
+    {
+            
+    }
+    public ClinicalTrialDbContext(DbContextOptions<ClinicalTrialDbContext> options) : base(options) 
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(GetConnectionstring());
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Models.ClinicalTrialDTO>()
+        modelBuilder.Entity<ClinicalTrialDTO>()
             .HasKey(c => c.Id);
 
-        modelBuilder.Entity<Models.ClinicalTrialDTO>()
+        modelBuilder.Entity<ClinicalTrialDTO>()
             .Property(c => c.Id)
             .ValueGeneratedOnAdd();
+    }
+
+    private static string GetConnectionstring()
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "localhost,1433",
+            UserID = "sa",
+            Password = "ctaPass1!",
+            TrustServerCertificate = true,
+        };
+        return builder.ConnectionString;
     }
 }
